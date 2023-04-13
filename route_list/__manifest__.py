3ca21635e210a67312caea62ae17302fcc501950
Alexander <113848382+PetrushinAlexander@users.noreply.github.com>
2023-04-13 09:55:01 +0300

    Add files via upload
    first commit

---
+++ route_list/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'route_list',
+    'version': '1.0',
+    'depends': ['base', 'product'],
+    'author': 'A.Petrushin',
+    'category': 'Uncategorized',
+    'description' : """
+    Creating PDF report via xml:
+    """,
+    'data': ['report/route_list_report.xml'],
+}
